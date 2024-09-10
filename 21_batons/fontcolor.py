dae2bc062ee0ac771fe98fe12e2f4bcbd9b84c1e
Ayckinn Lisa <160609756+AyckinnLisa@users.noreply.github.com>
2024-09-10 16:21:38 +0200

    Create fontcolor.py

---
+++ 21_batons/fontcolor.py
@@ -0,0 +1,16 @@
+# ----- FONT COLOR MODULE -----
+
+def red_font(txt):
+    return f"\033[91m{txt}\033[0m"
+
+def green_font(txt):
+    return f"\033[92m{txt}\033[0m"
+
+def yellow_font(txt):
+    return f"\033[93m{txt}\033[0m"
+
+def magenta_font(txt):
+    return f"\033[95m{txt}\033[0m"
+
+def blue_font(txt):
+    return f"\033[94m{txt}\033[0m"
